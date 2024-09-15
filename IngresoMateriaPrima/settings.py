10742959778170cbf737c6956cf0a107c00bda25
marcelo <mdllanes80@gmail.com>
2024-09-15 18:13:24 -0300

    se ingresan apps instaladas

--- IngresoMateriaPrima/settings.py
+++ IngresoMateriaPrima/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-simple-history',
+    'simple_history',
+    'app',
+    'porteria2',
+    'personal',
+    'proveedores',
+    'transporte',
 ]
 
 MIDDLEWARE = [
