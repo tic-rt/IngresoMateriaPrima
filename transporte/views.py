3f2f8dab6077fe9e717d95dea473fd6e745860c2
marcelo <mdllanes80@gmail.com>
2024-09-16 20:35:42 -0300

    se añade vista para transporte

--- transporte/views.py
+++ transporte/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from transporte.forms import FormCamion, FormTransporte
+from transporte.models import Camion, Semi, Transporte
+
 # Create your views here.
+
+def transportes(request):
+    """Esta funcion devolvera todas las empresas de transportes registradas"""
+    transportes = Transporte.objects.all()
+    camiones = Camion.objects.all()
+    semis = Semi.objects.all()
+    form_camion = FormCamion()
+    print(form_camion)
+    return render(request,'transporte/transportes.html',
+                  {'transportes':transportes,
+                   'camiones':camiones,
+                   'semis':semis,
+                   'form_camion':form_camion})
