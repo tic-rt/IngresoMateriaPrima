a2a8021f3bb270c94a2ab127030a6ec551a884c2
marcelo <mdllanes80@gmail.com>
2024-09-16 20:34:50 -0300

    se agrega un form de transporte

---
+++ transporte/forms.py
@@ -0,0 +1,34 @@
+
+from django import forms
+from django.forms import ModelForm
+
+from transporte.models import Camion, Transporte
+
+
+class FormTransporte(ModelForm):
+    
+    class Meta:
+        
+        model = Transporte
+        fields = [
+            'nombre'
+        ]
+        widgets = {
+            'nombre': forms.TextInput(attrs={'type':'text','class':'form-control'})
+        }
+        
+class FormCamion(ModelForm):
+
+    class Meta:
+        
+        model = Camion
+        fields = [
+            'transporte',
+            'marca',
+            'patente',
+            'vencimiento_seguro'
+        ]
+        widgets = {
+            'vencimiento_seguro':forms.DateInput(attrs={'type':'date','class':'form-control'})
+        }
+       
