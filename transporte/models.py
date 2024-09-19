f58be01c2edda52f0fa963a73b7551f19da5a59f
marcelo <mdllanes80@gmail.com>
2024-09-18 21:38:42 -0300

    se modifica nombre de campo para seguir convenciones

--- transporte/models.py
+++ transporte/models.py
@@ -54,7 +54,7 @@ class Semi(Base):
     
     transporte = models.ForeignKey(Transporte, on_delete=models.PROTECT, verbose_name='Transporte')
     patente = models.CharField(verbose_name='Patente', max_length=10, null=False, blank= False, default='AAA000AAA')
-    vencimientoSeguro = models.DateField(verbose_name='Vencimiento Seguro Semi') #Preguntar si va o no va 
+    vencimiento_seguro = models.DateField(verbose_name='Vencimiento Seguro Semi') #Preguntar si va o no va 
     
     class Meta:
         """Meta definicion para el Semi de una empresa de transporte"""
