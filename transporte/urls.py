0a7c1f3ec3b4f220742899990bedcef46ed316ef
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-09-19 13:44:43 -0300

    se añade ruta para agregar semi

--- transporte/urls.py
+++ transporte/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from transporte.views import agregarCamion, agregarTransporte, transportes
+from transporte.views import agregarCamion, agregarSemi, agregarTransporte, transportes
 
 
 urlpatterns = [
     path('',transportes,name='transportes'),
     path('agregarTransporte',agregarTransporte, name='agregarTransporte'),
     path('agregarCamion',agregarCamion,name='agregarCamion'),
+    path('agregarSemi',agregarSemi,name='agregarSemi')
 ]
