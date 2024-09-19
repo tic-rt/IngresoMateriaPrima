7a39ac42c0baddb5019c6c1e13799762f1fb937d
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-09-19 13:46:01 -0300

    se reorganiza codigo, se agrega nueva funcion

--- transporte/views.py
+++ transporte/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
-from transporte.forms import FormCamion, FormTransporte, formSemi
+from transporte.forms import FormCamion, FormSemi, FormTransporte
 from transporte.models import Camion, Semi, Transporte
 import sweetify
 
@@ -10,14 +10,16 @@ import sweetify
 def transportes(request):
     """Esta funcion devolvera todas las empresas de transportes registradas ,camiones y semis
         junto a los formularios correspondientes para la carga"""
+    
     transportes = Transporte.objects.all()
     camiones = Camion.objects.all()
     semis = Semi.objects.all()
+
     form_transporte = FormTransporte()
     form_camion = FormCamion()
-    form_semi = formSemi()
-    return render(request,'transporte/transportes.html',
-                  
+    form_semi = FormSemi()
+
+    return render(request,'transporte/transportes.html', 
                   {'transportes':transportes,
                    'camiones':camiones,
                    'semis':semis,
@@ -28,6 +30,7 @@ def transportes(request):
 
 def agregarTransporte(request):
     """esta funcion agrega una empresa de transporte nueva"""
+
     try:
         if request.method == 'POST':
             transporte = FormTransporte(request.POST)
@@ -45,15 +48,18 @@ def agregarTransporte(request):
                 errores_str = '<br>'.join(errores)
                 sweetify.warning(request,'Error al agregar empresa de transporte', text = errores_str, persistent = 'Aceptar')
                 return redirect('transportes')
+            
     except Exception as excepcion :
         sweetify.error('Error al agregar Empresa de transporte', persistent=f'ocurrio un error {str(excepcion)}')
         return redirect('transportes')
     
 def agregarCamion(request):
     """Esta funcion agrega un nuevo camion"""
+
     try:
         if request.method == 'POST':
             camion = FormCamion(request.POST)
+
             if(camion.is_valid()):
                 camion.save()
                 sweetify.toast(request,f'Camion patente {camion.clean_patente()} agregado', icon='success', timer=5000)
@@ -62,12 +68,37 @@ def agregarCamion(request):
                 errores = []
                 for campo, mensajes in camion.errors.items():
                     for mensaje in mensajes:
-                        errores.append(f'{campo}: {mensaje}')
-                
+                        errores.append(f'{campo}: {mensaje}')  
+
                 errores_str = '<br>'.join(errores)
                 sweetify.warning(request,'Error al agregar empresa de transporte', text = errores_str, persistent = 'Aceptar')
                 return redirect('transportes')
+            
     except Exception as excepcion:
         sweetify.error(request,'Error al agregar camion',persistent=f'ocurrio un error {str(excepcion)}')
         return redirect('transportes')
             
+def agregarSemi(request):
+    """Esta funcion agrega un nuevo semi"""
+
+    try:
+        if request.method == 'POST':
+            semi = FormSemi(request.POST)
+
+            if semi.is_valid():
+                semi.save()
+                sweetify.toast(request,f'Semi patente {semi.clean_patente()} agregado',icon='success', timer=5000)
+                return redirect('transportes')
+            else:
+                errores = []
+                for campo, mensajes in semi.errors.items():
+                    for mensaje in mensajes:
+                        errores.append(f'{campo}: {mensaje}')  
+
+                    errores_str = '<br>'.join(errores)
+                    sweetify.warning(request,'Error al agregar empresa de transporte', text = errores_str, persistent = 'Aceptar')
+                    return redirect('transportes')
+                
+    except Exception as excepcion:
+        sweetify.error(request,'Error al agregar camion',persistent=f'ocurrio un error {str(excepcion)}')
+        return redirect('transportes')
