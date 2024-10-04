24fe88a5697cf831d81c90ee17c9c2ca4e027675
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-10-04 14:58:19 -0300

    se añaden las rutas para conductor, y añadirConductor

--- transporte/urls.py
+++ transporte/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path
-
-from transporte.views import actualizar, agregarCamion, agregarSemi, agregarTransporte, camiones, editarCamion, editarSemi, editarTransporte, eliminarCamion, eliminarSemi, eliminarTransporte, semis, transportes  
+from transporte.views import (actualizar, agregarCamion, agregarConductor, agregarSemi, agregarTransporte, camiones, conductores, editarCamion, 
+                              editarSemi, editarTransporte, eliminarCamion, eliminarSemi, eliminarTransporte, semis, transportes)  
 
 
 urlpatterns = [
     path('', transportes, name='transportes'),
     path('camiones', camiones, name='camiones'),
     path('semis', semis, name='semis'),
+    path('conductores', conductores, name='conductores'),
     path('agregarTransporte', agregarTransporte, name='agregarTransporte'),
     path('agregarCamion', agregarCamion, name='agregarCamion'),
     path('agregarSemi', agregarSemi, name='agregarSemi'),
+    path('agregarConductor', agregarConductor, name='agregarConductor'),
     path('eliminarTransporte', eliminarTransporte, name = 'eliminarTransporte'),
     path('editarTransporte', editarTransporte, name='editarTransporte'),
+    path('eliminarCamion', eliminarCamion, name='eliminarCamion'),
+    path('eliminarSemi', eliminarSemi, name ='eliminarSemi'),
     path('editarCamion', editarCamion, name='editarCamion'),
     path('eliminarCamion', eliminarCamion, name='eliminarCamion'),
     path('editarSemi', editarSemi, name='editarSemi'),
-    path('eliminarSemi', eliminarSemi, name ='eliminarSemi'),
     path('actualizar/<str:modelo>',actualizar,name='actualizar'),
 ]
