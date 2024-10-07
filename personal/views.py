c3a30d8175478299e2572bd9a19157cb07a1ca75
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-10-07 12:57:17 -0300

    se agrega funcion personal a la vista

--- personal/views.py
+++ personal/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from personal.forms import FormPersonal
+from personal.models import Personal
+
 # Create your views here.
+
+def personal(request):
+    """Esta funcion devuelve todos los responsables de sector"""
+
+    responsables = Personal.objects.all()
+    form_responsable = FormPersonal()
+
+    return render(request,'personal/personal.html',
+                  {'responsables':responsables,
+                   'form_responsables':form_responsable})
