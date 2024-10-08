e327b96c1e014098ab74477e4fb986021da6bfb9
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-10-08 11:49:28 -0300

    se agrega sofdelete al modelo, definir si otras tablas haran referencia al id o guardaran los datos como string

--- personal/models.py
+++ personal/models.py
@@ -13,6 +13,7 @@ class Personal(models.Model):
     nombre = models.CharField(verbose_name='Nombre',null=False, blank=False, max_length=30)
     apellido = models.CharField(verbose_name='Apellido', null=False, blank=False, max_length=30)
     legajo = models.CharField(verbose_name='Nº Legajo', null=False,blank=False, unique=True, max_length=5)
+    is_deleted = models.BooleanField(verbose_name='Eliminado',default=False)
     
     class meta:
         """Meta definicion para Personal"""
@@ -22,3 +23,6 @@ class Personal(models.Model):
     def __str__(self):
         return f'{self.nombre} {self.apellido} {self.legajo} '
     
+    def delete(self, *args, **kwargs):#sof delete para mantener la integridad de la db
+        self.is_deleted=True
+        self.save()
