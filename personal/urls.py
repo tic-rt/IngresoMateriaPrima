4c65a8112e548ba66a5feb5f97c141b058c0f3d0
Marcelo Pc Fabrica <mdllanes80@gmail.com>
2024-10-08 11:50:01 -0300

    se agregan rutas para editar, eliminar y complementaria actualizar

--- personal/urls.py
+++ personal/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from personal.views import personal
+from personal.views import actualizar, agregarPersonal, editarPersonal, eliminarPersonal, personal
 
 
 urlpatterns = [
     path('', personal, name='personal'),
+    path('agregarPersonal/', agregarPersonal, name='agregarPersonal'),
+    path('editarPersonal/', editarPersonal, name='editarPersonal'),
+    path('eliminarpersonal',eliminarPersonal, name='eliminarPersonal'),
+    path('actualizar', actualizar, name='actualizar')
 ]
