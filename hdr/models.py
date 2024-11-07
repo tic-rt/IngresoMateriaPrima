b1a6d92dedd495d029ea7a87b7357d214cd40b4f
David LLanes <david.llanes@fm.gob.ar>
2024-11-07 12:50:11 -0300

    cambios menores

--- hdr/models.py
+++ hdr/models.py
@@ -11,15 +11,16 @@ sector = [
    ('PAMO/PSUL','PAMO/PSUL'),
 ]
 estado=[
-   ('Activo','Activo')
+   ('Activo','Activo'),
+   ('Anulado','Anulado'),
    ('Finalizado','Finalizado'),
    ('Rechazado','Rechazado')
 ]
 
 class HDR(Base):
-   fecha = models.DateTimeField(verbose_name='Feha')
-   sector = models.CharField(verbose_name= 'Sector',choices=sector, max_length=20)
-   estado=models.CharField(verbose_name='Estado',choices=estado, max_length=20)
+   fecha = models.DateTimeField(verbose_name='Feha', auto_now_add=True)
+   sector = models.CharField(verbose_name= 'Sector', choices=sector, max_length=20, default='Porteria 2')
+   estado=models.CharField(verbose_name='Estado', choices=estado, max_length=20, default='Activo')
    
    class Meta:
       """Meta defincion para HDR"""
