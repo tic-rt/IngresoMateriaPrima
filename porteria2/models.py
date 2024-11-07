8a50190097adc95df13145ada3e4538e1271977e
David LLanes <david.llanes@fm.gob.ar>
2024-11-07 12:51:32 -0300

    cambios menores en el modelo

--- porteria2/models.py
+++ porteria2/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from base.models import Base
+from hdr.models import HDR
 from personal.models import Personal
 from proveedores.models import Producto
 from transporte.models import Camion, Semi, Transporte, Conductor
@@ -9,16 +10,17 @@ from transporte.models import Camion, Semi, Transporte, Conductor
 class Ingreso(Base):
     """Esta clase representa el Ingreso de un vehiculo para descarga en la porteria es 1 de n etapas de la HDR"""
 
-    horaIngreso = models.DateTimeField(verbose_name="Hora de Ingreso",auto_now_add=True)
-    ingresoCalefaccion = models.BooleanField(verbose_name='Ingreso para calefaccion de cisterna')
-    empresaTransporte = models.ForeignKey(Transporte,on_delete=models.PROTECT)
+    hora_ingreso = models.DateTimeField(verbose_name="Hora de Ingreso",auto_now_add=True)
+    ingreso_calefaccion = models.BooleanField(verbose_name='Ingreso para calefaccion de cisterna')
+    empresa_transporte = models.ForeignKey(Transporte,on_delete=models.PROTECT)
     conductor = models.ForeignKey(Conductor,on_delete=models.PROTECT) 
     producto = models.ForeignKey(Producto,on_delete=models.PROTECT)
     remito = models.CharField(verbose_name='Remito',blank=False, null=False, max_length=100)
-    patenteChasis = models.ForeignKey(Camion,on_delete=models.PROTECT)
-    patenteSemi  = models.ForeignKey(Semi, on_delete=models.PROTECT)
+    patente_chasis = models.ForeignKey(Camion,on_delete=models.PROTECT)
+    patente_semi  = models.ForeignKey(Semi, on_delete=models.PROTECT)
     responsable = models.ForeignKey(Personal, on_delete=models.PROTECT)
     ingresado = models.BooleanField(verbose_name='Ingresado',null=False, default=False)
+    id_hdr = models.ForeignKey(HDR, on_delete=models.PROTECT)
     
     class Meta:
         """Meta definicion para la clase Ingreso"""
