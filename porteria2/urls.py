b0c2ea809ae3ef9e18463df9869cf88f7554a67c
David LLanes <david.llanes@fm.gob.ar>
2024-11-14 13:19:14 -0300

    se añade url para control de epp

--- porteria2/urls.py
+++ porteria2/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from porteria2.views import guardarNuevoIngreso, index, nuevoIngreso
+from porteria2.views import controlEpp, guardarNuevoIngreso, index, nuevoIngreso
 
 
 urlpatterns = [
-    path('',index,name='porteria2'),
-    path('nuevoIngreso',nuevoIngreso,name='nuevoIngreso'),
-    path('guardarNuevoIngreso',guardarNuevoIngreso,name='guardarNuevoIngreso'),
+    path('', index, name='porteria2'),
+    path('nuevoIngreso', nuevoIngreso, name='nuevoIngreso'),
+    path('guardarNuevoIngreso', guardarNuevoIngreso, name='guardarNuevoIngreso'),
+    path('controlEpp', controlEpp, name='ControlEpp'),
 ]
