707fc246118933d76d1a16bcb3f1942ad3a0303e
David LLanes <david.llanes@fm.gob.ar>
2024-11-21 11:45:03 -0300

    se añade comentario  de rechazo a hoja de ruta

--- hdr/models.py
+++ hdr/models.py
@@ -21,7 +21,7 @@ class HDR(Base):
    fecha = models.DateTimeField(verbose_name='Feha', auto_now_add=True)
    sector = models.CharField(verbose_name= 'Sector', choices=sector, max_length=20, default='Porteria 2')
    estado=models.CharField(verbose_name='Estado', choices=estado, max_length=20, default='Activo')
-   #falta observacion por si anulo o rechazo
+   observacion = models.CharField(verbose_name='Observaciones', max_length=200,blank=True, default='')
    
    class Meta:
       """Meta defincion para HDR"""
