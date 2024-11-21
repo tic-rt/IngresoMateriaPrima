c23a9912df5fc316322efaffa0ef9b1dd108c22b
David LLanes <david.llanes@fm.gob.ar>
2024-11-21 12:13:57 -0300

    se añade widget para comentario en hdr

---
+++ hdr/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from hdr.models import HDR
+
+
+class FormHDR(forms.ModelForm):
+    model = HDR
+    fields =['observacion']
+
+    widgets={'observacion': forms.Textarea(attrs={'class':'form-control',
+                                                  'row':10,
+                                                  'cols':50,
+                                                  'placeholder':'Observacion',
+                                                  'maxlength':200,
+                                                  'autofocus':'autofocus'})}
