df6198a9eef4bed7b3dd81e3085ba49ab7dd23da
David LLanes <david.llanes@fm.gob.ar>
2024-12-20 10:52:11 -0300

    agregadas vistas para pamo y psul

--- pamo/views.py
+++ pamo/views.py
@@ -1,15 +1,66 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 import sweetify
 
+from hdr.models import HDR
+from pamo.forms import formPamo
 from porteria2.models import Ingreso
 
 # Create your views here.
 def pendientes(request):
     """muestra los camiones pendientes de control que provienen de IQ"""
     try:
-        ingresos = Ingreso.objects.filter(ingresado = True, is_deleted = False, hdr_sector ='PAMO/PSUL' )
-        return render('pamo/pendientes.html',{'ingresos':ingresos})
+        ingresos = Ingreso.objects.filter(ingresado = True, is_deleted = False, hdr__sector ='PAMO')
+        return render(request, 'pamo/pendientes.html',{'ingresos':ingresos})
     
     except Exception as excepcion:
         sweetify.error(request, 'Error', text=f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
         return redirect ('index')
+
+def controlPamo(request):
+    """control de presion y temperatura para pamo"""
+    try:
+        if request.method == 'GET':
+            id_ingreso = request.GET.get('id_ingreso')
+            ingreso = Ingreso.objects.get(id = id_ingreso, is_deleted = False , ingresado = True , hdr__estado = 'Activo', hdr__sector = 'PAMO')
+            formPAMO = formPamo()
+            return render (request, 'pamo/control.html',{'ingreso':ingreso,'formPAMO':formPAMO})
+        else:
+            return redirect('pendientesPAMO')
+    except Exception as excepcion:
+        sweetify.error(request, 'Error', text = f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
+        return redirect('pendientesPAMO')
+
+@login_required
+def guardarPamo(request):
+    try:
+        if request.method == 'POST':
+            id_ingreso = request.POST.get('id_ingreso')
+            existe_ingreso = Ingreso.objects.filter(id = id_ingreso,ingresado = True, hdr__sector = 'PAMO', hdr__estado = 'Activo').exists()
+
+            if existe_ingreso :
+                ingreso = Ingreso.objects.get(id = id_ingreso,ingresado = True, hdr__sector = 'PAMO', hdr__estado = 'Activo')
+                form_pamo = formPamo(request.POST)
+                form_pamo.instance.hdr = ingreso.hdr
+                
+                if form_pamo.is_valid(): 
+                    hdr = HDR.objects.get(id = ingreso.hdr.id)  
+                    hdr.sector =  'Almacen PQ E'
+                    hdr.save()
+                    form_pamo.save()            
+                    sweetify.success(request,'Guardado', text='Control de presion y temperatura guardados correctamente', timer=3000)
+                    return redirect('pendientesPAMO')
+                else:
+                    sweetify.warning(request,'Error', text='Debe completar todos los campos', timer = 3000)
+                    return render(request, 'pamo/control.html', {'ingreso':ingreso,
+                                                                 'formPAMO':form_pamo})
+            else:
+                sweetify.error(request, 'Error', text='El ingreso referenciado no existe o fue eliminado', persistent = 'Aceptar')
+                return redirect('pendientesPAMO')
+            
+        else:
+            return redirect('pendientesPAMO')
+    except Exception as excepcion:
+        sweetify.error(request, 'Error', text=f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
+        return redirect('pendientesPAMO')
+        
--- psul/views.py
+++ psul/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+import sweetify
+
+from hdr.models import HDR
+from psul.forms import formPsul
+from porteria2.models import Ingreso
 
 # Create your views here.
+def pendientes(request):
+    """muestra los camiones pendientes de control que provienen de IQ"""
+    try:
+        ingresos = Ingreso.objects.filter(ingresado = True, is_deleted = False, hdr__sector ='PSUL')
+        return render(request, 'psul/pendientes.html',{'ingresos':ingresos})
+    
+    except Exception as excepcion:
+        sweetify.error(request, 'Error', text=f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
+        return redirect ('index')
+
+def controlPsul(request):
+    """control de presion y temperatura para psul"""
+    try:
+        if request.method == 'GET':
+            id_ingreso = request.GET.get('id_ingreso')
+            ingreso = Ingreso.objects.get(id = id_ingreso, is_deleted = False , ingresado = True , hdr__estado = 'Activo', hdr__sector = 'PSUL')
+            formPSUL = formPsul()
+            return render (request, 'psul/control.html',{'ingreso':ingreso,'formPSUL':formPSUL})
+        else:
+            return redirect('pendientesPSUL')
+    except Exception as excepcion:
+        sweetify.error(request, 'Error', text = f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
+        return redirect('pendientesPSUL')
+
+@login_required
+def guardarPsul(request):
+    try:
+        if request.method == 'POST':
+            id_ingreso = request.POST.get('id_ingreso')
+            existe_ingreso = Ingreso.objects.filter(id = id_ingreso,ingresado = True, hdr__sector = 'PSUL', hdr__estado = 'Activo').exists()
+
+            if existe_ingreso :
+                ingreso = Ingreso.objects.get(id = id_ingreso,ingresado = True, hdr__sector = 'PSUL', hdr__estado = 'Activo')
+                form_psul = formPsul(request.POST)
+                form_psul.instance.hdr = ingreso.hdr
+                
+                if form_psul.is_valid(): 
+                    hdr = HDR.objects.get(id = ingreso.hdr.id)  
+                    hdr.sector =  'Almacen PQ E'
+                    hdr.save()
+                    form_psul.save()            
+                    sweetify.success(request,'Guardado', text='Control de presion y temperatura guardados correctamente', timer=3000)
+                    return redirect('pendientesPSUL')
+                else:
+                    sweetify.warning(request,'Error', text='Debe completar todos los campos', timer = 3000)
+                    return render(request, 'psul/control.html', {'ingreso':ingreso,
+                                                                 'formPSUL':form_psul})
+            else:
+                sweetify.error(request, 'Error', text='El ingreso referenciado no existe o fue eliminado', persistent = 'Aceptar')
+                return redirect('pendientesPSUL')
+            
+        else:
+            return redirect('pendientesPSUL')
+    except Exception as excepcion:
+        sweetify.error(request, 'Error', text=f'Ocurrio un error {str(excepcion)}', persistent = 'Aceptar')
+        return redirect('pendientesPSUL')
+        
