2d688a87127cfdf78b20f46b237d3d9829da49ff
RT / David Llanes (TIC) <david.llanes@fm.gob.ar>
2025-12-23 08:01:37 -0300

    se añaden apis para obtener info de cantidad de camiones y camiones en transito

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from app.views import index
+from app.views import cantidad_camiones, index,transito
 
 
 urlpatterns = [
     path('',index,name='index'),
+    path('cantidad_camiones/',cantidad_camiones,name='cantidad_camiones'),
+    path('transito/',transito,name='transito'),
 ]
