34f37b6208cee5226cc44b2fe3c4a742acfd8d03
RT / David Llanes (TIC) <david.llanes@fm.gob.ar>
2026-01-08 09:48:49 -0300

    se añaden vistas para HDR

--- hdr/views.py
+++ hdr/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from porteria2.models import Ingreso
+
 # Create your views here.
+
+def lista_hdr(request):
+    """Funcion que devuelve la vista principal de hdr"""
+    hdr = None
+    try:
+        hdr = Ingreso.objects.filter(ingresado = True)
+    except Exception as excepcion:
+        pass
+    
+    return render(request,'HDR/listaHDR.html',{'HDR':hdr})
+
+def exportar_hdr(request):
+    """Funcion que exporta la lista de hdr a pdf"""
+    return render(request,'HDR/HDR.html')
