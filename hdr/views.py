efc52624efd09e1fb760b5424ef72200d472d426
RT / David Llanes (TIC) <david.llanes@fm.gob.ar>
2026-07-22 09:11:31 -0300

    se elimina paginacion, no corresponde a este feature

--- hdr/views.py
+++ hdr/views.py
@@ -25,15 +25,7 @@ def lista_hdr(request):
     hdr = None
     try:
         hdr = Ingreso.objects.filter().all()
-        
-        paginador = Paginator(hdr, 10)
-        page = request.GET.get('page', 1)
-        page_number = request.GET.get('page')
-        
-        if page_number is None:
-            page_number = 1 
-            
-        hdr = paginador.get_page(page)
+
     except Exception as excepcion:
         pass
     
